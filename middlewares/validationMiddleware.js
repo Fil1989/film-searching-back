49fe4285d4c85a92a1b6d4e911d2d1e1a21e6bfb
Filip Koza <38352025+Fil1989@users.noreply.github.com>
2021-10-24 19:59:51 +0200

    Update validationMiddleware.js

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const validateUser = async (req, res, next) => {
   const schema = Joi.object({
     password: Joi.string().min(6).max(30).required(),
-    email: Joi.string().email().required().max(50),
+    email: Joi.string().email({ tlds: { allow: false } }).required().max(50),
     name: Joi.string().required().min(3).max(30),
   })
 
@@ -17,7 +17,7 @@ const validateUser = async (req, res, next) => {
 const validateLogin = async (req, res, next) => {
   const schema = Joi.object({
     password: Joi.string().min(6).max(30).required(),
-    email: Joi.string().email().required().max(50),
+    email: Joi.string().email({ tlds: { allow: false } }).required().max(50),
   })
 
   const validationResult = schema.validate(req.body)
